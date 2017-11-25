b8985bf1fd6c6a6d5b17166ad8be8d03cf343e3b
Alex Dixon <alex@conarrative.com>
2017-11-25 07:04:45 -0800

    Make state tree table and collapsible

--- precept-visualizer/src/cljs/precept_visualizer/views/state_tree.cljs
+++ precept-visualizer/src/cljs/precept_visualizer/views/state_tree.cljs
@@ -1,34 +1,65 @@
 (ns precept-visualizer.views.state-tree
-  (:require [precept.core :as precept]))
+  (:require [precept.core :as precept]
+            [reagent.core :as r]))
+
+
+(defn elided-e [x]
+  (if (keyword? x)
+    [:span (str x)]
+    (let [elided? (r/atom true)]
+      (fn []
+       (if @elided?
+         [:span {:on-click #(swap! elided? not)}
+           (str (subs(str x) 0 6)
+            "...")]
+         [:span {:on-click #(swap! elided? not)}
+           (str x)])))))
+
+
+(defn row [[e a v]]
+  [:tr {:key (str e "-" a "-" (hash v))}
+   [:td [elided-e e]]
+   [:td (str a)]
+   [:td {:on-click #(println "collapse/expand")}
+    [:div (with-out-str (cljs.pprint/pprint v))]]])
+
 
 ;(defn state-tree [*orm-states])
 ;; What to name???
 (defn main [*orm-states]
   (let [sub @(precept/subscribe [:state-tree])
         tree (get @*orm-states (:state/number sub))
+        collapsed? (r/atom false)
         _ (println (count @*orm-states))
-        _ (println (:state/number sub))
-        _ (println "state-tree render" tree)]
-    [:div
-     [:h4 "State tree"]
-     [:div {:style {:display "flex" :justify-content "space-between"}}
-      [:div "e"]
-      [:div "a"]
-      [:div "v"]]
-     (map (fn [[e av]]
-            ^{:key (str e)}
-            [:div {:style {:display "flex"}}
-             [:div {:style {:margin-right 15}}
-              (str e)]
-             [:div {:style {:display "flex" :flex 1 :flex-direction "column"}}
-              (map
-                (fn [[a v]]
-                  ^{:key (str e "-" a "-" (hash v))}
-                  [:div {:style {:min-width "100%" :display "flex" :justify-content "space-between"}}
-                   [:div {:style {:flex 1}}
-                    (str a)]
-                   [:div {:style {:flex 1}}
-                    (with-out-str (cljs.pprint/pprint v))]])
-                av)]])
-       tree)]))
-
+        _ (println (:state/number sub))]
+    (fn [*orm-states]
+      [:div {:style {:margin-left 24}}
+       [:h4 {:on-click #(do (println "click")
+                            (reset! collapsed? (not @collapsed?)))}
+        "State tree ^"]
+       (when (not @collapsed?)
+         [:table
+          [:thead
+           [:tr
+            [:th "e"]
+            [:th {:style {:width "30%"}}
+             "a"]
+            [:th "v"]]]
+          (map-indexed
+            (fn [i [e av]]
+              [:tbody
+                 {:key (str e i)
+                  :style {:background-color (if (even? i) "#888" "#fff")
+                          :color (if (even? i) "#fff" "#000")}}
+                (concat
+                  (list ^{:key (str e av i)} [row [e (ffirst av) (second (first av))]])
+                  (map
+                    (fn [[a v]]
+                      [:tr {:key (str a v i)}
+                        [:td
+                         ""]
+                        [:td (str a)]
+                        [:td {:on-click #(println "collapse/expand")}
+                          [:div (with-out-str (cljs.pprint/pprint v))]]])
+                    (rest av)))])
+           tree)])])))
