df6692380807e8c891c6bc7be38adc8a51c8d5ea
Alex Dixon <alex@conarrative.com>
2017-12-30 07:53:32 -0800

    Move explanation rules into fact-tracking ns

--- precept-visualizer/src/cljs/precept_visualizer/rules/core.cljs
+++ precept-visualizer/src/cljs/precept_visualizer/rules/core.cljs
@@ -3,14 +3,12 @@
   (:require [precept.rules :refer [rule define session defsub]]
             [precept.util :refer [insert! retract! insert-unconditional!] :as util]
             [precept.accumulators :as acc]
-            [precept.orm :as orm]
             [precept-visualizer.themes :as themes]
-            [precept-visualizer.mouse :as mouse]
-            [precept-visualizer.state :as state]
             [precept-visualizer.schema :refer [db-schema client-schema]]
             [precept-visualizer.event-parser :as event-parser]
     ;; TODO. Rename other-rules ns
             [precept-visualizer.rules.rule-tracking]
+            [precept-visualizer.rules.fact-tracking]
             [precept-visualizer.other-rules]
             [precept-visualizer.ws :as ws]))
 
@@ -51,32 +49,6 @@
                           (merge {:db/id ::themes/light} themes/light)]))
 
 
-(rule on-clear-all-explanations
-  {:group :action}
-  [[:transient :clear-all-explanations true]]
-  [?explanations <- (acc/all) :from [_ :explaining/fact]]
-  =>
-  (retract! ?explanations))
-
-
-;; TODO. Not sure if we should pull all eids associated with the
-;; fact-str or leave them. No apparent problems with leaving them (current approach)
-(rule on-clear-explanation
-  {:group :action}
-  [[:transient :stop-explain-fact-requested ?fact-str]]
-  [?fact-exp <- [_ :explaining/fact ?fact-str]]
-  =>
-  (retract! ?fact-exp))
-
-
-(rule on-explain-request
-  {:group :action}
-  [[:transient :explanation/request ?fact-str]]
-  [:not [_ :explaining/fact ?fact-str]]
-  =>
-  (insert-unconditional! [(util/guid) :explaining/fact ?fact-str]))
-
-
 (rule when-sync-tracking-latest-state
   {:group :action}
   [[:global :tracking/sync? true]]
@@ -96,60 +68,6 @@
 
 (define [:windows :main/width-percent (- 100 ?explanation-width)]
         :- [[:windows :explanations/width-percent ?explanation-width]])
-
-
-
-(rule explain-fact-for-events-in-state
-  ;; when explaining a fact
-  [[?explanation-id :explaining/fact ?fact-str]]
-  ;; and we are tracking a state number
-  [[:global :tracking/state-number ?current-state-number]]
-
-  ;; and current state has event with that fact's value
-  [[?state-id :state/number ?current-state-number]]
-  [[?state-id :state/events ?event-id]]
-  [[?event-id :event/facts ?fact-id]]
-  [[?fact-id :fact/string ?fact-str]]
-  [[?event-id :event/number ?event-number]]
-  ;; when no explanation exists for a fact mentioned in event of current state
-  [:not [?event-id :event/log-entry]]
-
-  =>
-  ;; get event object to generate explanation
-  (ws/get-log-entry-by-coords [?current-state-number ?event-number]))
-
-
-(rule form-event-explanation-when-requested-and-log-entry
-  [[?explanation-id :explaining/fact ?fact-str]]
-  ;; and we are tracking a state number
-  [[:global :tracking/state-number ?current-state-number]]
-
-  ;; and current state has event with that fact's value
-  [[?state-id :state/number ?current-state-number]]
-  [[?state-id :state/events ?event-id]]
-  [[?event-id :event/facts ?fact-id]]
-  [[?fact-id :fact/string ?fact-str]]
-  [[?event-id :event/log-entry ?log-entry]]
-  =>
-  ;; Because this is already a data representation of an event there's not
-  ;; much we can do to make it into an explanation here. View will format as needed
-  ;; Note: request-id isn't effectively used -- could make inserted fact one-to-many
-  ;; and get rid of it
-  ;; We are performing this insertion so we can accumulate all explanations
-  ;; in a subscription. Logically it's not necessary; we could just have this in the
-  ;; sub if we had better matching and marshalling capability with lists
-  ;; note :event/log-entry is a base fact, pulled from server on demand and
-  ;; kept in memory. This duplicate of it should come and go based upon the
-  ;; presence of an explanation request, hence insertL here. :explanation
-  ;; implies "active"
-  (insert! [?explanation-id :explanation/log-entry {:fact-str ?fact-str
-                                                    :event ?log-entry}]))
-
-
-(defsub :explanations
-  [?explanations <- (acc/all :v) :from [_ :explanation/log-entry]]
-  =>
-  {:payload ?explanations})
 
 
 (defsub :selected-theme
@@ -207,6 +125,7 @@
 (session visualizer-session
   'precept-visualizer.rules.core
   'precept-visualizer.rules.rule-tracking
+  'precept-visualizer.rules.fact-tracking
   'precept-visualizer.other-rules
   :db-schema db-schema
   :client-schema client-schema
