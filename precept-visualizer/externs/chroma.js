571b46d0d4291fa4f08a980ee55f0edaa3efbb56
Alex Dixon <alex@conarrative.com>
2018-05-19 09:40:11 -0700

    Add chroma externs file

---
+++ precept-visualizer/externs/chroma.js
@@ -0,0 +1,41 @@
+var chroma = function () {}
+chroma.version = {}
+chroma._input = {}
+chroma.brewer = {}
+chroma.colors = {}
+chroma.lab = function () {}
+chroma.bezier = function () {}
+chroma.cubehelix = function () {}
+chroma.random = function () {}
+chroma.rgb = function () {}
+chroma.hex = function () {}
+chroma.hsl = function () {}
+chroma.hsv = function () {}
+chroma.num = function () {}
+chroma.css = function () {}
+chroma.lch = function () {}
+chroma.hcl = function () {}
+chroma.cmyk = function () {}
+chroma.gl = function () {}
+chroma.interpolate = function () {}
+chroma.mix = function () {}
+chroma.kelvin = function () {}
+chroma.temperature = function () {}
+chroma.contrast = function () {}
+chroma.blend = {
+  normal: function () {},
+  multiply: function () {},
+  screen: function () {},
+  overlay: function () {},
+  darken: function () {},
+  lighten: function () {},
+  dodge: function () {},
+  burn: function () {}
+}
+chroma.analyze = function () {}
+chroma.scale = function () {}
+chroma.scales = {}
+chroma.limits = function () {}
+chroma.hsi = function () {}
+chroma.luminance = function () {}
+
